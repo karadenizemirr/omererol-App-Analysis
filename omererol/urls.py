f8108c4d3067b627fce4b5a2ff9d2995cf81d95c
karadenizemirr <karadenizemirr@gmail.com>
2023-09-05 12:34:57 +0300

    Deploy

--- omererol/urls.py
+++ omererol/urls.py
@@ -20,4 +20,4 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('', include('pages.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
